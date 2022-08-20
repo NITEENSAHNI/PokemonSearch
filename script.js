254da6b6cab5f4cc30c4f1a81fe17f4c1f97bf50
Niteen <niteensahni@gmai.com>
2022-08-20 16:46:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let tosearch;
+let PokeArray = [];
+let images ;
+let pokename;
+let i = 0;
+
+function search()
+{   
+    tosearch = document.getElementById("tosearch").value 
+    tosearch = tosearch.toLowerCase();
+    document.getElementById("pokedex").innerHTML=tosearch.toUpperCase();
+    console.log(tosearch)
+    
+    fetch('https://pokeapi.co/api/v2/pokemon/'+tosearch).then((resolve)=>resolve.json()).then((PokeonObject)=>
+    {
+        
+       images = PokeonObject.sprites.other["official-artwork"].front_default;
+       console.log("this",images)
+    }).catch((error)=>{
+        console.log("pokemon not found 404",error)
+    })
+   document.getElementById("image").style.backgroundImage =`url("${images}")`;
+   document.getElementById("container").style.backgroundColor="teal";
+   console.log("images=",images)
+
+}
